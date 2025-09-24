21793c9256b4450a63710cc481aaf46158221f6d
AshenafiGever <ashenfigenene@gmail.com>
2025-09-24 10:57:37 +0300

    updater function applied on counterusing react hooksk

--- src/Components/CounterUsingHooksUseState.jsx
+++ src/Components/CounterUsingHooksUseState.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
+import "./Counter.css";
 
 function CounterUsingHooksUseState() {
-  console.log(useState("evangaditech"));
+  // console.log(useState("evangaditech"));
+  const [bootCamp,setBootcamp]=useState("Evanagadi")
 
   let [count, setCount] = useState(0);
-  const increment = () => {};
+  const increment = () => {
+    count= count+10//never use this it never update it at the interface part it increase in cosole.log(count)
+    console.log(count);
+   setCount((x)=> x+10);//use this upadter funxtion since it upade clearly on the interface
+  };
   return (
-    <div>
+    <div className="">
+      <h1>{bootCamp}</h1>
+      <h1>{setBootcamp}</h1>
       <h1> UseState Example</h1>
-      <h1>{count}</h1>
-      <h1>{setCount}</h1> <button onClick={increment}> Increment By ten</button>
+      <h1>{count} </h1>
+      <h1>{setCount}</h1>{" "}
+      <button className="green" onClick={increment}>
+        {" "}
+        Increment By ten
+      </button>
     </div>
   );
 }
