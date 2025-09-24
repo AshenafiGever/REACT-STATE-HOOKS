9c95fdb6a04ae9683e2a0c3b39a1dc847e30e4bc
binitutor <ashenafigenene@gmail.com>
2025-09-24 17:57:20 +0300

    added counterhome.css & useStatefor count added

--- src/Components/CounterUsingHooksUseState.jsx
+++ src/Components/CounterUsingHooksUseState.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
+import './Counterhome.css'
 
 function CounterUsingHooksUseState() {
   console.log(useState("evangaditech"));
+  const [bootCamp,setBootcamp]= useState("TopEvangadi")
+
+  console.log(bootCamp)
 
   let [count, setCount] = useState(0);
-  const increment = () => {};
+  const increment = () => {
+    // count= count+10;
+    setCount((x)=> x+10);
+  };
   return (
     <div>
       <h1> UseState Example</h1>
+      <h1> {bootCamp}</h1>
       <h1>{count}</h1>
-      <h1>{setCount}</h1> <button onClick={increment}> Increment By ten</button>
+      <h1>{setCount}</h1> <button  className="green" onClick={increment}> Increment By ten</button>
     </div>
   );
 }
