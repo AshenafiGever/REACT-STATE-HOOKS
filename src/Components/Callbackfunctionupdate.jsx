d766a7d2bdfc0d2d7896ad99c18a0090800f407a
AshenafiGever <ashenfigenene@gmail.com>
2025-09-25 17:14:12 +0300

    upadater function with prev to include previous data with the updating

--- src/Components/Callbackfunctionupdate.jsx
+++ src/Components/Callbackfunctionupdate.jsx
@@ -10,10 +10,14 @@ function Callbackfunctionupdate() {
       "https://images-stag.jazelc.com/uploads/garycrossleyford-m2en/Screen-Shot-2023-08-10-at-7.57.48-AM-1024x590.png",
   });
   const updateColor=()=>{
-    setCar({color:"blue",
-
-
-   }) };
+//     setCar({color:"blue",
+//       image: "https://i.ytimg.com/vi/hwhVza9y0QU/maxresdefault.jpg"
+// //wrong way--> never update state directly!! right work on some situiation ,but it is not recommeded.(we will lose all other data)
+//RIGHT WAY rather we have to use callback function is the right to not lose previous date
+setCar((prevState)=>({...prevState,color:"blue",
+image: "https://i.ytimg.com/vi/hwhVza9y0QU/maxresdefault.jpg"
+   }) );
+  };
   return (
     <div>
       <h1>WHY using CALLBACK function is good when updating states</h1>
@@ -28,7 +32,7 @@ function Callbackfunctionupdate() {
       </div>
       <button type="button" onClick={updateColor}>
         change color to blue
-      </button>
+      </button> 
     </div>
   );
 }
